cf32c7edfd0bd14d44c69d59c103272d509fe3fb
rajjeet <rajjeet.phull@gmail.com>
2018-02-10 14:44:57 -0500

    Added gig viewmodel

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,13 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
-        // GET: Gigs
-        public ActionResult Create()
+
+        private readonly ApplicationDbContext _context;
+
+        public GigsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
-    }
+        
+        public ActionResult Create()
+        {            
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    } 
 }
