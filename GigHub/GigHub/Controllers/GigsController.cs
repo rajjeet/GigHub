2c70ea5ee6b7508e2deb149b6712c47a53a6faf8
rajjeet <rajjeet.phull@gmail.com>
2018-02-10 14:44:57 -0500

    Added gigs form UI and view model

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,13 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+
+        private ApplicationDbContext _context;
+        
         // GET: Gigs
         public ActionResult Create()
         {
-            return View();
+            _context = new ApplicationDbContext();
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
         }
-    }
+    } 
 }
