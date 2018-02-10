ef064059a9a37182d8c1a2eb83821980bdeb673d
rajjeet <rajjeet.phull@gmail.com>
2018-02-10 16:12:26 -0500

    Added post method to GigsController

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -15,13 +17,31 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
         
+        [Authorize]
         public ActionResult Create()
         {            
             var viewModel = new GigFormViewModel
-            {
+            { 
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                 ArtistId = User.Identity.GetUserId(),  
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     } 
 }
