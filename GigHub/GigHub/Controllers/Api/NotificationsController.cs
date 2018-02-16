73a08ea6c540d8e40aa0acc0b665c38729b6b17d
rajjeet <rajjeet.phull@gmail.com>
2018-02-15 19:51:43 -0500

    Added notification controller and get call

---
+++ GigHub/GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,35 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        [Authorize]
+        public IEnumerable<Notification> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            return notifications;
+        }
+
+    }
+}
