52eeaf40279955ab75575e11745bd622f2583add
rajjeet <rajjeet.phull@gmail.com>
2018-02-20 15:36:17 -0500

    Refactored js code from gigs view to apps js

---
+++ GigHub/GigHub/Scripts/app/app.js
@@ -0,0 +1,46 @@
+﻿var initGigsAttendance = function () {
+    $(".js-toggle-attendance").click(function (e) {
+        var button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                .done(function () {
+                    button
+                        .removeClass("btn-default")
+                        .addClass("btn-info")
+                        .text("Going");
+                })
+                .fail(function () {
+                    alert("Something failed!");
+                });
+        } else {
+            $.ajax({
+                url: "/api/attendances/" + button.attr("data-gig-id"),
+                method: "DELETE"
+            })
+                .done(function () {
+                    button
+                        .removeClass("btn-info")
+                        .addClass("btn-default")
+                        .text("Going?");
+                })
+                .fail(function () {
+                    alert("Something failed!");
+                });
+        }
+    });
+}
+
+
+var initGigsFollowing = function () {
+    $(".js-toggle-follow").click(function (e) {
+        var button = $(e.target);
+        $.post("/api/followings", { followeeId: button.attr("data-followee-id") })
+            .done(function () {
+                button
+                    .text("Unfollow");
+            })
+            .fail(function () {
+                alert("Something failed!");
+            });
+    });
+}
