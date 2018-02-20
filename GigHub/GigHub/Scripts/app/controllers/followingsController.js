42d8a7fac9d3d27aa2f34d863525bee5455b2ca5
rajjeet <rajjeet.phull@gmail.com>
2018-02-20 17:27:23 -0500

    Refactored following js using modules and physical seperation

---
+++ GigHub/GigHub/Scripts/app/controllers/followingsController.js
@@ -0,0 +1,30 @@
+﻿var followingController = function (followingsService) {
+    var button;
+
+    var init = function () {
+        $(".js-toggle-follow").click(function (e) {
+            button = $(e.target);
+
+            var followeeId = button.attr("data-followee-id");
+
+            if (button.hasClass("btn-default"))
+                followingsService.createFollowing(followeeId, done, fail);
+            else
+                followingsService.deleteFollowing(followeeId, done, fail);
+        });
+    }
+
+    var fail = function () {
+        alert("Something failed!");
+    };
+
+    var done = function () {
+        var text = button.text() === "Follow" ? "Following" : "Follow";
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    }
+
+    return {
+        init: init
+    }
+
+}(FollowingsService);
