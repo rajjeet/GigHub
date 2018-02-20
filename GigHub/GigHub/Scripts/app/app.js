429b637a185e461c8c322f845c4d892000d1d664
rajjeet <rajjeet.phull@gmail.com>
2018-02-20 16:02:35 -0500

    Refactored done and fail functions in gigs controller in app js

--- GigHub/GigHub/Scripts/app/app.js
+++ GigHub/GigHub/Scripts/app/app.js
@@ -1,51 +1,47 @@
-﻿var GigsController = function() {
+﻿var GigsController = function () {
+    var button;
 
     var initAttendance = function () {
-        $(".js-toggle-attendance").click(function (e) {
-            var button = $(e.target);
-            if (button.hasClass("btn-default")) {
-                $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
-                    .done(function () {
-                        button
-                            .removeClass("btn-default")
-                            .addClass("btn-info")
-                            .text("Going");
-                    })
-                    .fail(function () {
-                        alert("Something failed!");
-                    });
-            } else {
-                $.ajax({
-                        url: "/api/attendances/" + button.attr("data-gig-id"),
-                        method: "DELETE"
-                    })
-                    .done(function () {
-                        button
-                            .removeClass("btn-info")
-                            .addClass("btn-default")
-                            .text("Going?");
-                    })
-                    .fail(function () {
-                        alert("Something failed!");
-                    });
-            }
-        });
+        $(".js-toggle-attendance").click(toggleAttendance);
+    };
+
+    toggleAttendance = function (e) {
+        button = $(e.target);
+        if (button.hasClass("btn-default")) {
+            $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                .done(done)
+                .fail(fail);
+        } else {
+            $.ajax({
+                    url: "/api/attendances/" + button.attr("data-gig-id"),
+                    method: "DELETE"
+                })
+                .done(done)
+                .fail(fail);
+        }
 
     };
 
-    var initFollowing = function() {
+    var initFollowing = function () {
         $(".js-toggle-follow").click(function (e) {
-            var button = $(e.target);
+            button = $(e.target);
             $.post("/api/followings", { followeeId: button.attr("data-followee-id") })
                 .done(function () {
                     button
                         .text("Unfollow");
-                })
-                .fail(function () {
-                    alert("Something failed!");
-                });
+                }) 
+                .fail(fail);
         });
 
+    };
+
+    var done = function () {
+        var text = (button.text() === "Going") ? "Going?" : "Going";
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    var fail = function() {
+        alert("Something failed!");
     };
 
     return {
