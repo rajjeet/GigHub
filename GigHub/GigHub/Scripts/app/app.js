1295477bde1a107e575beb0696f636419cf2582e
rajjeet <rajjeet.phull@gmail.com>
2018-02-20 15:47:47 -0500

    Refactored app.js to modualize init functions

--- GigHub/GigHub/Scripts/app/app.js
+++ GigHub/GigHub/Scripts/app/app.js
@@ -1,46 +1,56 @@
-﻿var initGigsAttendance = function () {
-    $(".js-toggle-attendance").click(function (e) {
-        var button = $(e.target);
-        if (button.hasClass("btn-default")) {
-            $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+﻿var GigsController = function() {
+
+    var initAttendance = function () {
+        $(".js-toggle-attendance").click(function (e) {
+            var button = $(e.target);
+            if (button.hasClass("btn-default")) {
+                $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                    .done(function () {
+                        button
+                            .removeClass("btn-default")
+                            .addClass("btn-info")
+                            .text("Going");
+                    })
+                    .fail(function () {
+                        alert("Something failed!");
+                    });
+            } else {
+                $.ajax({
+                        url: "/api/attendances/" + button.attr("data-gig-id"),
+                        method: "DELETE"
+                    })
+                    .done(function () {
+                        button
+                            .removeClass("btn-info")
+                            .addClass("btn-default")
+                            .text("Going?");
+                    })
+                    .fail(function () {
+                        alert("Something failed!");
+                    });
+            }
+        });
+
+    };
+
+    var initFollowing = function() {
+        $(".js-toggle-follow").click(function (e) {
+            var button = $(e.target);
+            $.post("/api/followings", { followeeId: button.attr("data-followee-id") })
                 .done(function () {
                     button
-                        .removeClass("btn-default")
-                        .addClass("btn-info")
-                        .text("Going");
+                        .text("Unfollow");
                 })
                 .fail(function () {
                     alert("Something failed!");
                 });
-        } else {
-            $.ajax({
-                url: "/api/attendances/" + button.attr("data-gig-id"),
-                method: "DELETE"
-            })
-                .done(function () {
-                    button
-                        .removeClass("btn-info")
-                        .addClass("btn-default")
-                        .text("Going?");
-                })
-                .fail(function () {
-                    alert("Something failed!");
-                });
-        }
-    });
-}
+        });
 
+    };
 
-var initGigsFollowing = function () {
-    $(".js-toggle-follow").click(function (e) {
-        var button = $(e.target);
-        $.post("/api/followings", { followeeId: button.attr("data-followee-id") })
-            .done(function () {
-                button
-                    .text("Unfollow");
-            })
-            .fail(function () {
-                alert("Something failed!");
-            });
-    });
-}
+    return {
+        initAttendance: initAttendance,
+        initFollowing: initFollowing
+    };
+}();
+
