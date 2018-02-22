d5bb51ced81bae0c6ebdf6b223ce3cc492f110b4
rajjeet <rajjeet.phull@gmail.com>
2018-02-22 06:53:16 -0500

    Setup of test for api gig controller

--- GigHub/GigHub/Persistance/Repositories/GigsRepository.cs
+++ GigHub/GigHub/Persistance/Repositories/GigsRepository.cs
@@ -60,5 +60,7 @@ namespace GigHub.Persistance.Repositories
         {
             _context.Gigs.Add(gig);
         }
+
+        
     }
 }
---
+++ GigHub/Gighub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using GigHub.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Gighub.Tests.Controllers.Api
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+
+
+        public GigsControllerTests()
+        {
+            var identity = new GenericIdentity("user1@domain.com");
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "user1@domain.com"));
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "1"));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockUoW = new Mock<IUnitOfWork>();
+            
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
