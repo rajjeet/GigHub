3e2be6403e719f791faf1422861c49a6db464e24
rajjeet <rajjeet.phull@gmail.com>
2018-02-23 07:37:45 -0500

    Added few unit test for gig repository using ef6

--- GigHub/Gighub.Tests/Persistance/Repositories/GigRepositoryTests.cs
+++ GigHub/Gighub.Tests/Persistance/Repositories/GigRepositoryTests.cs
@@ -15,6 +15,8 @@ namespace Gighub.Tests.Persistance.Repositories
     {
         private GigsRepository _repository;
         private Mock<DbSet<Gig>> _mockGigs;
+        private string _artistId;
+        private Gig[] gigs;
 
         [TestInitialize]
         public void TestInitialize()
@@ -22,7 +24,7 @@ namespace Gighub.Tests.Persistance.Repositories
             _mockGigs = new Mock<DbSet<Gig>>();
             var mockContext =  new Mock<IApplicationDbContext>();
             mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
-
+            _artistId = "2";
             _repository = new GigsRepository(mockContext.Object);
         }
 
@@ -44,6 +46,27 @@ namespace Gighub.Tests.Persistance.Repositories
             _mockGigs.SetSource(new[] { gig } );
             var result = _repository.GetGigsForArtist("1");
             result.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetGigsForArtist_GigIsForDifferentArtist_ShouldNotBeReturned()
+        {            
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(1), ArtistId = _artistId };        
+
+            _mockGigs.SetSource(new[] { gig });
+            var result = _repository.GetGigsForArtist(gig.ArtistId + "-");
+            result.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetGigsForArtist_GigIsForGivenArtistAndIsInTheFuture_ShouldBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(1), ArtistId = _artistId };
+
+            gigs = new[] { gig };
+            _mockGigs.SetSource(gigs);
+            var result = _repository.GetGigsForArtist(gig.ArtistId);
+            result.Should().Equal(gigs);
         }
 
     }
