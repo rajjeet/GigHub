8a6aebd5b4068e91cfc993f121930a4861b51831
rajjeet <rajjeet.phull@gmail.com>
2018-02-23 21:41:21 -0500

    Adapted an extention method to mock users for controller integration tests

---
+++ GigHub/GigHub.IntegrationTests/Extensions/ControllerExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.IntegrationTests.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static void MockCurrentUser(this Controller controller, string userId, string username)
+        {
+            var identity = new GenericIdentity(username);
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", username));
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockHttpContent = new Mock<HttpContextBase>();
+            mockHttpContent.SetupGet(hc => hc.User).Returns(principal);
+
+            var mockControllerContext = new Mock<ControllerContext>();
+            mockControllerContext.SetupGet(c => c.HttpContext).Returns(mockHttpContent.Object);
+
+            controller.ControllerContext = mockControllerContext.Object;
+
+        }
+    }
+}
