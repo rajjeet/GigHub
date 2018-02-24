24e228214091d9499be25915627d50f431faa66d
rajjeet <rajjeet.phull@gmail.com>
2018-02-23 21:27:05 -0500

    Added attribute for isolating integration test

---
+++ GigHub/GigHub.IntegrationTests/Isolated.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Transactions;
+
+namespace GigHub.IntegrationTests
+{
+    public class Isolated : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+
+        public void BeforeTest(TestDetails testDetails)
+        {
+            _transactionScope = new TransactionScope();
+        }
+
+        public void AfterTest(TestDetails testDetails)
+        {
+            _transactionScope.Dispose();
+        }
+
+        public ActionTargets Targets => ActionTargets.Test;
+    }
+}
